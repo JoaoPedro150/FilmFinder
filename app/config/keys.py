710647ef2f605239eff36eb35f23c0884b1ee203
JoaoPedro150 <jpedroo150@gmail.com>
2018-11-20 10:52:33 -0200

    Add heroku requirements

---
+++ app/config/keys.py
@@ -0,0 +1,6 @@
+import os
+
+NGROK_KEY = str(os.getenv("NGROK_KEY"))
+TMDB_KEY = str(os.getenv("TMDB_KEY"))
+ACCESS_TOKEN = str(os.getenv("ACCESS_TOKEN"))
+VERIFY_TOKEN = str(os.getenv("VERIFY_TOKEN"))
